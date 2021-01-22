4b9862b53e0649c5694a918fd4ac228123147c7b
leonardo <antunesleo4@gmail.com>
2021-01-22 15:31:57 -0300

    Add thought model, create test case to publish a thought add add sharethoughts to installed apps

--- sharethoughts/models.py
+++ sharethoughts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Thought(models.Model):
+    thought = models.CharField(max_length=800)
+    created_at = models.DateTimeField(auto_now_add=True)
--- sharethoughts/tests.py
+++ sharethoughts/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+from sharethoughts.models import Thought
+
+
+def create_thought(thought: str) -> Thought:
+    return Thought(thought=thought)
+
+
+class ThoughtsListViewTest(TestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_should_publish_a_thought(self):
+        data = {'thought': '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in lectus vitae elit congue interdum sed ut quam. Vivamus at lectus lacus. Proin fringilla porta sapien, vel scelerisque quam aliquet at. Integer faucibus justo nibh, sodales placerat eros consectetur sed. Praesent ultrices felis arcu, at luctus turpis auctor fermentum. '''}
+        response = self.client.post(reverse('thoughts-list'), data, format='json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(1, Thought.objects.count())
+        thought = Thought.objects.get()
+        self.assertEqual(data['thought'], thought.thought)
